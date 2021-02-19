29754c3a3422a42120d621f3cd65d98ce735b336
PreethiN0070 <Preethinarayan2003@gmail.com>
2021-02-19 10:11:57 +0000

    finding out maximum and minimum numbers

--- Program_3.c
+++ Program_3.c
@@ -9,6 +9,14 @@
 #include<stdio.h> 
 int main()
 {
+  int row,col,array a(2D array),i;
+  printf("enter the number of rows:");
+  scanf("%d",&row);
+  printf("enter the number of columns:");
+  scanf("%d",&col);
+  for(row=0;row<i;i++);
+   printf("enter the elements in row%d\n");
+   for
   //Declare all the variables[row, col, array a(2D array), i] used in the code
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
    //Read the elements of matrix a using nested for loop and scanf statment
--- Program_5.c
+++ Program_5.c
@@ -6,18 +6,21 @@
 int main()
 {
     int arr[5],n, i,max,min;
+    printf("enter the array elements ");
     scanf("%d", &n);
-
+ 
     for(i = 0; i < n; i++)
-    
-        scanf("%d",&arr[i]);
-        max=arr[0];
-        min=arr[0];
-    
-    
-    for(i=1;i<n;i++)
     {
-      if(arr[i] > max)
+      scanf("%d",&arr[i]);
+    }
+        
+    max=arr[0];
+    min=arr[0];
+    
+    
+    for(i=0;i<n;i++)
+    {
+      if(arr[i] >max)
        max=arr[i];
        if(arr[i] < min)
        min=arr[i];
